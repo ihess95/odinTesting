0654d8ca5a9111f795b197e04ba0d01bc44f0d31
Ian Hess <ian.h.hess@gmail.com>
2023-02-07 15:18:18 -0500

    start writing test?

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const someOrder = {
-  items: [
-    { name: "Dragon Food", price: 8, quantity: 8 },
-    { name: "Dragon Cage (small)", price: 800, quantity: 2 },
-    { name: "Shipping", price: 40, shipping: true },
-  ],
-};
+if (
+  orderTotal({
+    items: [
+      { name: "Dragon Food", price: 8, quantity: 8 },
+      { name: "Dragon Cage (small)", price: 800, quantity: 2 },
+    ],
+  }) !== 808
+) {
+  throw new Error("Check fail: Happy Path");
+}
 
-const orderTotal = (order) => {
-  const totalItems = order.items
-    .filter((x) => !x.shipping)
-    .reduce((prev, cur) => prev + cur.price * cur.quantity, 0);
-  const shippingItem = order.items.find((x) => !!x.shipping);
-  const shipping = totalItems > 1000 ? 0 : shippingItem.price;
-  return totalItems + shipping;
-};
+// const someOrder = {
+//     { name: "Shipping", price: 40, shipping: true },
+//   ],
+// };
 
-result = orderTotal(someOrder);
+// const orderTotal = (order) => {
+//   const totalItems = order.items
+//     .filter((x) => !x.shipping)
+//     .reduce((prev, cur) => prev + cur.price * cur.quantity, 0);
+//   const shippingItem = order.items.find((x) => !!x.shipping);
+//   const shipping = totalItems > 1000 ? 0 : shippingItem.price;
+//   return totalItems + shipping;
+// };
+
+// result = orderTotal(someOrder);
