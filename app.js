e17987864569fc1b40904c2dac3d8bc159f0d2fd
Ian Hess <ian.h.hess@gmail.com>
2023-02-08 13:01:09 -0500

    start jest tutorial

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ if (
     ],
   }) !== 808
 ) {
-  throw new Error("Check fail: Happy Path");
+  throw new Error("Check fail: Happy Path (Example 1)");
+}
+if (
+  orderTotal({
+    items: [
+      { name: "Dragon collar", price: 20 },
+      { name: "Dragon chew toy", price: 40 },
+    ],
+  }) !== 60
+) {
+  throw new Error("Check fail: Happy Path (2)");
+}
+
+function orderTotal(prder) {
+  return order.items.reduce((prev, cur) => cur.price + prev, 0);
 }
 
 // const someOrder = {
