08e58a31987c4c2db13fa0047fad39dab2c2dba7
Ian Hess <ian.h.hess@gmail.com>
2023-02-09 16:26:43 -0500

    write other tests

--- odin/app.test.js
+++ odin/app.test.js
@@ -22,3 +22,12 @@ const calculator = require("./app");
 test("returns the math asked for", () => {
   expect(calculator(10, 3, "+")).toBe(13);
 });
+test("returns the math asked for", () => {
+  expect(calculator(100, 9, "-")).toBe(91);
+});
+test("returns the math asked for", () => {
+  expect(calculator(2, 8, "*")).toBe(16);
+});
+test("returns the math asked for", () => {
+  expect(calculator(40, 4, "/")).toBe(10);
+});
